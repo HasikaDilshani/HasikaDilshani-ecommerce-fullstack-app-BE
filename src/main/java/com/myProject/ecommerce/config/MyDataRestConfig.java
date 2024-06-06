4a689ff06871d185c92e28373d2d9f258da9161b
Hasika <hasikadilshani@gmail.com>
2024-06-06 23:34:36 +0530

    project added

---
+++ src/main/java/com/myProject/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.myProject.ecommerce.config;
+
+import com.myProject.ecommerce.entity.Product;
+import com.myProject.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry registry) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        //disable write operations for product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+        //disable write operations for product category
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+
+}
---
+++ src/main/java/com/myProject/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.myProject.ecommerce.dao;
+
+import com.myProject.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
